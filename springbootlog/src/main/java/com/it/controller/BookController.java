e2920f1a5fa762c284c46164de42e10fc197b358
hongsheng <1634340724@qq.com>
2022-08-25 18:20:24 +0800

    日志操作

--- springbootlog/src/main/java/com/it/controller/BookController.java
+++ springbootlog/src/main/java/com/it/controller/BookController.java
@@ -1,17 +1,20 @@
 package com.it.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/books")
+@Slf4j
 public class BookController {
 
     //创建记录日志的对象
-    private  static  final Logger log=  LoggerFactory.getLogger(BookController.class);
+   // private  static  final Logger log=  LoggerFactory.getLogger(BookController.class);
     @GetMapping
     public String get(){
         log.debug("debug");
