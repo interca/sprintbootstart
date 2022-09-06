5ef116395baf7905201dc2597e6ac4e5dee537c3
hongsheng <1634340724@qq.com>
2022-09-06 15:02:14 +0800

    plus

--- mybatis-plus/src/test/java/com/it/MybatisPlusApplicationTests.java
+++ mybatis-plus/src/test/java/com/it/MybatisPlusApplicationTests.java
@@ -1,5 +1,8 @@
 package com.it;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.it.dto.AccountDto;
@@ -8,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @SpringBootTest
@@ -54,4 +58,52 @@ class MybatisPlusApplicationTests {
 		System.out.println(page.getRecords());
 	}
 
+	@Test
+	void  test2(){
+		//按条件查询
+		QueryWrapper wrapper=new QueryWrapper();
+		//id小于十的用户
+		wrapper.lt("id",10);
+		List<AccountDto> accountDtos = accountMapper.selectList( wrapper);
+		for(AccountDto k:accountDtos){
+			System.out.println(k);
+		}
+	}
+
+	@Test
+	void  test3(){
+		//按条件查询
+		QueryWrapper wrapper=new QueryWrapper();
+		//多条件查询
+		wrapper.lt("id",14);
+		wrapper.gt("id",10);
+		List<AccountDto> accountDtos = accountMapper.selectList( wrapper);
+		for(AccountDto k:accountDtos){
+			System.out.println(k);
+		}
+	}
+
+	@Test
+	void  test4(){
+		//按条件查询
+		LambdaQueryWrapper<AccountDto> wrapper=new LambdaQueryWrapper<>();
+		//多条件查询或者关系
+		wrapper.lt(AccountDto::getId,10).or().gt(AccountDto::getId,14);
+		List<AccountDto> accountDtos = accountMapper.selectList( wrapper);
+		for(AccountDto k:accountDtos){
+			System.out.println(k);
+		}
+	}
+
+	@Test
+	void  test5(){
+		//按条件查询
+		LambdaQueryWrapper<AccountDto> wrapper=new LambdaQueryWrapper<>();
+		//等匹配
+		wrapper.eq(AccountDto::getName,"牛牛");
+		List<AccountDto> accountDtos = accountMapper.selectList( wrapper);
+		for(AccountDto k:accountDtos){
+			System.out.println(k);
+		}
+	}
 }
