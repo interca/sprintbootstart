8e36deaed510f30328a2cc5767657dc7af8bc69d
hongsheng <1634340724@qq.com>
2022-09-07 19:04:35 +0800

    结构初始化

--- spring-security/src/test/java/com/it/SpringSecurityApplicationTests.java
+++ spring-security/src/test/java/com/it/SpringSecurityApplicationTests.java
@@ -1,13 +1,22 @@
 package com.it;
 
+import com.it.domain.User;
+import com.it.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
     @Test
     void contextLoads() {
+        List<User> userList = userMapper.selectList(null);
+        System.out.println(userList);
     }
 
 }
